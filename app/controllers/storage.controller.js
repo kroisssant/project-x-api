24f901bff6d2fdeb4e6166bb9a3005a859a98db9
unknown <davidneacsu8@gmail.com>
2021-06-27 18:40:14 +0300

    comit

--- app/controllers/storage.controller.js
+++ app/controllers/storage.controller.js
@@ -1,16 +1,18 @@
-const multer = require('multer')
-var storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'uploads/profilepics')
-    },
-    filename: function(req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, file.fieldname + '.' + file.mimetype)
-    }
-})
+// //go to storege.routes.js for the controller
+// const multer = require('multer')
+//     // var storage = multer.diskStorage({
+//     //     destination: function(req, file, cb) {
+//     //         cb(null, 'uploads/profilepics/')
+//     //     },
+//     //     filename: function(req, file, cb) {
+//     //         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+//     //         cb(null, file.fieldname)
+//     //     }
+//     // })
 
-var upload = multer({ storage: storage })
+// var upload = multer({ dest: 'uploads/' })
 
-exports.profilePic = (req, res) => {
-    console.log(req)
-}
+// exports.profilePic = (req, res) => {
+//     console.log("yes")
+//     upload.single("img")
+// }
--- app/routes/storage.routes.js
+++ app/routes/storage.routes.js
@@ -1,7 +1,23 @@
 const controller = require('../controllers/storage.controller')
 const { authJwt } = require("../middlewares");
+const path = require("path")
+
+
+// multer 
 const multer = require('multer')
-var upload = multer({ dest: 'uploads/' })
+    //storage for profilepics
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/profilepics/')
+    },
+    filename: function(req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + uniqueSuffix + path.extname(file.originalname))
+    }
+})
+const upload = multer({ storage: storage })
+
+
 
 module.exports = function(app) {
     app.use(function(req, res, next) {
@@ -12,6 +28,5 @@ module.exports = function(app) {
         next();
     });
 
-    app.post("/api/storage/profilePics", controller.profilePic, [authJwt.verifyToken], upload.single("img"))
-
+    app.post("/api/storage/profilePics", [authJwt.verifyToken], upload.single("img"))
 };
